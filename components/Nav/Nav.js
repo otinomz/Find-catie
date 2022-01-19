f0c29745be5071c6c3d5ca7082d743b5ab36d667
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:48:47 +0100

    feature: code refactor

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Nav() {
-    return <nav className="navar navbar-light bg-light">
-        <div className="container-fluid">
-            <span className="navbar-brand mb-0 h1">Navbbar</span>
-      </div>
-  </nav>;
+    return (
+        <nav className="navar navbar-light bg-light">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Navbbar</span>
+            </div>
+        </nav>
+    )
 }

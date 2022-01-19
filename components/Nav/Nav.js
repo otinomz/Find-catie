7f55f962d1793ec246c53390e864fb0ca287005b
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:25:44 +0100

    feature: bootsraping app with bootstrap components

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function Nav() {
-    return <div>
-      
-  </div>;
+    return <nav className="navar navbar-light bg-light">
+        <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Navbbar</span>
+      </div>
+  </nav>;
 }

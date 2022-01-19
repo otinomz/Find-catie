f48c64fe5396f550d8138834b22f59c3db7b986e
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:38:15 +0100

    feature: bootstrap installed

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Nav from '../components/Nav/Nav'
 import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
   return (

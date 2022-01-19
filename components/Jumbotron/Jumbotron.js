4e98db0bc4b8b6c1366f986798e1433d6bbcab22
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:54:00 +0100

    code cleanup

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -4,8 +4,8 @@ export default function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid mt-5">
             <div className="container">
-            <h1 className="display-4">Fluid jumbotron</h1>
-            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <h1 className="display-4">Nala World</h1>
+            <p className="lead">Find your new best friend today</p>
             </div>
         </div>
     )

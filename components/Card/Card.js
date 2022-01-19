2f44d48bc6c18a41df703b5b4afb955eb915cbd4
otinomz <awesomeotinomz@gmail.com>
2022-01-19 19:38:59 +0100

    feature: passed props

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from "Card.module.css"
 
-export default function Card({name, phone, email}) {
+export default function Card({id, name, phone, email, image}) {
     return (
         <div
             className={styles.card}
         >
             <div className={styles["card-header"]}>
-                <img src="" alt="" className={styles["card-img"]} />
+                <img src={image.url} alt={image.alt} className={styles["card-img"]} />
             </div>
             <div>
-                <h3></h3>
-                <p></p>
-                <p></p>
+                <h3>{name}</h3>
+                <p>{phone}</p>
+                <p>{email}</p>
             </div>
         </div>
     )

af109ff4683373a77e4e458a0cfd81bb8796adbf
otinomz <awesomeotinomz@gmail.com>
2022-01-19 19:36:26 +0100

    feature: new imports

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from "Card.module.css"
 
-export default function Card() {
+export default function Card({name, phone, email}) {
     return (
         <div
             className={styles.card}
         >
             <div className={styles["card-header"]}>
                 <img src="" alt="" className={styles["card-img"]} />
+            </div>
+            <div>
+                <h3></h3>
+                <p></p>
+                <p></p>
             </div>
         </div>
     )

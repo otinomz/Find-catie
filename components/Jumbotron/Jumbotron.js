b39f2cc66bf3c9f455a8049126aa6c46dcdd0da8
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:56:37 +0100

    feature: button added

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -4,8 +4,9 @@ export default function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid mt-5">
             <div className="container">
-            <h1 className="display-4">Nala World</h1>
-            <p className="lead">Find your new best friend today, adopt a Cat</p>
+                <h1 className="display-4">Nala World</h1>
+                <p className="lead">Find your new best friend today, adopt a Cat</p>
+                <button type="button" className="btn btn-primary">Primary</button>
             </div>
         </div>
     )

67853e0ad5feafa9f607d9137dc478cd07a7176d
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:52:33 +0100

    feature: code cleanup

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Jumbotron() {
     return (
-        <div className="jumbotron jumbotron-fluid">
+        <div className="jumbotron jumbotron-fluid mt-5">
             <div className="container">
             <h1 className="display-4">Fluid jumbotron</h1>
             <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>

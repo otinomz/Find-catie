f13d6b0ad0905a4cc76c1d4c973cbd03ec54aaae
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:50:41 +0100

    feature: importing Jumotron components to the main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Jumbotron from '../components/Jumbotron/Jumbotron'
 import Nav from '../components/Nav/Nav'
 import styles from '../styles/Home.module.css'
 
@@ -13,7 +14,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Nav/>
+        <Nav />
+        <Jumbotron/>
       </main>
       
     </div>

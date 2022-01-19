e15760f5405d84456b9d21aa9053465a7e761444
otinomz <awesomeotinomz@gmail.com>
2022-01-19 19:09:05 +0100

    feature: rendering the navar at the cat page

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Nav from '../../components/Nav/Nav';
 
 export default function index() {
     return (
         <div>
-            <h1>Cats</h1>
+            <Nav/>
         </div>
     )
 }

7418f1c3f3fbb9d587cd075599c3106b280b0cc8
otinomz <awesomeotinomz@gmail.com>
2022-01-19 18:39:50 +0100

    feature: link added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Nav from '../components/Nav/Nav'
 import styles from '../styles/Home.module.css'
-import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
   return (

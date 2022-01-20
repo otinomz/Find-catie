fe840030e40cb3f787c4705fb497e551bb5c5c54
otinomz <awesomeotinomz@gmail.com>
2022-01-20 12:20:17 +0100

    feature: code cleanup

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Jumbotron from '../components/Jumbotron/Jumbotron'
 import Nav from '../components/Nav/Nav'
+import Default from '../layouts/Default'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,10 +14,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Nav />
-        <Jumbotron/>
-      </main>
+        <Default>
+          <Jumbotron/>
+        </Default>
+        
+      
       
     </div>
   )

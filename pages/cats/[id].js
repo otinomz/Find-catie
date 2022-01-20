a67e4e6099b2c85de2397d6f45a1fa4ba81cc64a
otinomz <awesomeotinomz@gmail.com>
2022-01-20 12:03:12 +0100

    feature: code cleanup

--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -25,7 +25,7 @@ export default function Cat() {
             {/*rendering cat information for specific cat in mind */}
             <div>
                 {cat && (
-                    <div className="mt-5 flex">
+                    <div className="container mt-5 flex">
                         <img src={cat.image.url} alt={cat.image.alt} />
                         <div className="mx-5">
                             <h1>{cat.name}</h1>

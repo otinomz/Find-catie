f0018a8d10b8e379194d2c294cfbda8e03221c40
otinomz <awesomeotinomz@gmail.com>
2022-01-20 19:00:14 +0100

    feat: code cleanup

--- layouts/Default.js
+++ layouts/Default.js
@@ -5,9 +5,11 @@ export default function Default({children}) {
     return (
         <div>
             <Nav />
+            <p>Designed by Otinomo</p>
             <main>
                 {children}
             </main>
+
         </div>
     )
 }

36ed9ed7fcd6d0c76735786479623fe7a036e8a1
otinomz <awesomeotinomz@gmail.com>
2022-01-20 18:26:28 +0100

    feat: code cleanup

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        {/* default layout  */}
+        {/* default layout housing the nav */}
         <DefaultLayout>
           <Jumbotron/>
         </DefaultLayout>

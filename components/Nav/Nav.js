a49b66c1b31e733c585c75b7ce310f79b57a561d
otinomz <awesomeotinomz@gmail.com>
2022-01-20 11:18:09 +0100

    feature: code refactor

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function Nav() {
     return (
-        <nav className="navar navbar-light bg-light">
+        <nav className="navar navbar-light bg-light mt-5">
             <div className="container-fluid">
                 <Link href="/" className="navbar-brand mb-0 h1">Navbbar</Link>
             </div>

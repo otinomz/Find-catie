9fbc655a09e9a0ef65ddeba0497635a24007d939
otinomz <awesomeotinomz@gmail.com>
2022-01-20 11:41:42 +0100

    feature: more and more and more

--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function Cat() {
+    const router = useRouter()
+    const fetchCat = async () => {
+
+    }
+
 
     return (
         // rendering cat information for specific cat in mind

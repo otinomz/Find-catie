a7e69a1da16a36f166b90550dcbe601bfeb6c7e2
otinomz <awesomeotinomz@gmail.com>
2022-01-20 11:23:29 +0100

    feature: routing

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from "./Card.module.css"
+import Router from 'next/router';
 
 export default function Card({id, name, phone, email, image}) {
     return (
         <div
             className={styles.card}
+            onClick={()=> Router.push("/id")}
         >
             <div className={styles["card-header"]}>
                 <img src={image.url} alt={image.alt} className={styles["card-img"]} />
--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function Jumbotron() {
+    const router = useRouter()
+
     return (
         <div className="jumbotron jumbotron-fluid mt-5">
             <div className="container">
                 <h1 className="display-4">Nala World</h1>
                 <p className="lead">Find your new best friend today, adopt a Cat</p>
-                <button type="button" className="btn btn-primary">Start Looking</button>
+                <button type="button"
+                    className="btn btn-primary"
+                    onClick={() => router.push("/cats")}
+                >
+                    Start Looking
+                </button>
             </div>
         </div>
-    )
+    )  
 }
  

33adee16bdcb6d2c59f366473b7e03d9ed729118
otinomz <awesomeotinomz@gmail.com>
2022-01-20 10:18:44 +0100

    feature: querying data

--- pages/api/cats/[id].js
+++ pages/api/cats/[id].js
@@ -2,6 +2,9 @@
 import {cats} from "../../../data"
 
 export default function handler(req, res) {
-  const {id} = req.query 
-  res.status(200).json({hello: id })
+  const { id } = req.query 
+  
+  const cat = cats.find(cat => cat.id === Number(id))
+  
+  res.status(200).json({ hello: id })
 }

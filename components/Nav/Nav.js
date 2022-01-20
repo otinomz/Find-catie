cbb499a251cc1c8f27afe1252ef67c3b75bcd1bb
otinomz <awesomeotinomz@gmail.com>
2022-01-20 18:57:23 +0100

    feat: code cleanup

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
     return (
         <nav className="navar navbar-light bg-light mt-5">
             <div className="container-fluid">
-                <Link href="/" className="navbar-brand mb-0 h1">Navbbar</Link>
+                <Link href="/" style={{textDecoration: "none"}} className="navbar-brand mb-0 h1">HomePage</Link>
             </div>
         </nav>
     )

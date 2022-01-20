8951b7716c8c9a3c9bf02ac4ad3847f365ef1d9f
otinomz <awesomeotinomz@gmail.com>
2022-01-20 11:39:41 +0100

    feature: rendering the specific description for individual cat

--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -3,8 +3,9 @@ import React from 'react';
 export default function Cat() {
 
     return (
+        // rendering cat information for specific cat in mind
         <div>
-            cat yo
+            
         </div>
         
     ) 
